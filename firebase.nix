16254887dbbd11363843ae13c4a909e8178b4d68
Nick Hamilton <git@adventjah.net>
2018-01-08 07:26:49 +0100

    WIP

--- firebase.nix
+++ firebase.nix
@@ -1,31 +1,30 @@
 { mkDerivation, aeson, attoparsec, base, bytestring, criterion
-, data-default, directory, either, firebase-collegevine, generic-random
-, hashids, HsOpenSSL, hspec, http-client, http-client-tls
-, http-streams, http-types, io-streams, lens, MissingH, mtl
-, nano-http, QuickCheck, safe, servant, servant-client
-, servant-server, SHA, split, stdenv, string-conversions, text
-, text-show, time, unordered-containers, uuid, wai, warp
+, data-default, directory, either, firebase-collegevine
+, generic-random, hashids, HsOpenSSL, hspec, http-client
+, http-client-tls, http-streams, http-types, io-streams, lens
+, MissingH, mtl, nano-http, QuickCheck, safe, servant
+, servant-client, servant-server, SHA, split, stdenv
+, string-conversions, text, text-show, time, unordered-containers
+, uuid, wai, warp
 }:
 mkDerivation {
   pname = "firebase";
   version = "0.0.1";
   src = ./.;
-  isLibrary = false;
-  isExecutable = true;
-  executableHaskellDepends = [
+  libraryHaskellDepends = [
     aeson attoparsec base bytestring data-default directory either
-    firebase-collegevine generic-random hashids HsOpenSSL hspec http-client
-    http-client-tls http-streams http-types io-streams lens MissingH
-    mtl nano-http QuickCheck safe servant servant-client servant-server
-    SHA split string-conversions text text-show time
+    firebase-collegevine generic-random hashids HsOpenSSL hspec
+    http-client http-client-tls http-streams http-types io-streams lens
+    MissingH mtl nano-http QuickCheck safe servant servant-client
+    servant-server SHA split string-conversions text text-show time
     unordered-containers uuid wai warp
   ];
   testHaskellDepends = [
     aeson attoparsec base bytestring data-default directory either
-    firebase-collegevine generic-random hashids HsOpenSSL hspec http-client
-    http-client-tls http-streams http-types io-streams lens MissingH
-    mtl nano-http QuickCheck safe servant servant-client servant-server
-    SHA split string-conversions text text-show time
+    firebase-collegevine generic-random hashids HsOpenSSL hspec
+    http-client http-client-tls http-streams http-types io-streams lens
+    MissingH mtl nano-http QuickCheck safe servant servant-client
+    servant-server SHA split string-conversions text text-show time
     unordered-containers uuid wai warp
   ];
   benchmarkHaskellDepends = [ base criterion ];
